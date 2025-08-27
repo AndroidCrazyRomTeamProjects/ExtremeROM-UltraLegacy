45f4493f83a3954598cff49ad81d81f4974fed58
Android Artisan <romartisan2025@gmail.com>
2025-08-27 17:38:54 +0200

    unica: fixup and brand

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -1,8 +1,7 @@
 TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
 # Set build ID
-VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "ExtremeROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "ArtisanROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME"
 
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
